4b494215b8410ea605a7ce4b2168f6fe6eb3791d
AppTv <apptv.contato@gmail.com>
2021-04-12 17:18:38 -0300

    Alterado Splash
    Alterado Splash

--- app/src/main/java/com/example/apphistoria/Lista01Activity.java
+++ app/src/main/java/com/example/apphistoria/Lista01Activity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
